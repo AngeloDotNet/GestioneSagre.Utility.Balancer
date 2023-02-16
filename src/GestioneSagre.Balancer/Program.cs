a7b9c0edad42a2226836ec686c4ca2a70611789c
Angelo Pirola <angelo9871@gmail.com>
2023-02-16 20:17:13 +0100

    Primo commit

---
+++ src/GestioneSagre.Balancer/Program.cs
@@ -0,0 +1,19 @@
+namespace GestioneSagre.Balancer;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+
+        Startup startup = new(builder.Configuration);
+
+        startup.ConfigureServices(builder.Services);
+
+        var app = builder.Build();
+
+        startup.Configure(app);
+
+        app.Run();
+    }
+}
---
+++ src/GestioneSagre.Balancer/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace GestioneSagre.Balancer;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+    }
+
+    public IConfiguration Configuration { get; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddReverseProxy()
+            .LoadFromConfig(Configuration.GetSection("ReverseProxy"));
+    }
+
+    public void Configure(WebApplication app)
+    {
+        IWebHostEnvironment env = app.Environment;
+
+        app.UseRouting();
+        app.UseEndpoints(endpoints =>
+        {
+            endpoints.MapReverseProxy();
+        });
+    }
+}
